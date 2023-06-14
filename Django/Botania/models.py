ab7e8c308062dc6cdaba5b4c4eaf8a40edad9a55
FioFrigerio <106987298+FioFrigerio@users.noreply.github.com>
2023-06-14 10:27:53 -0400

    Add files via upload

--- Django/Botania/models.py
+++ Django/Botania/models.py
@@ -2,16 +2,30 @@ from django.db import models
 
 # Create your models here.
 
-class Usuario(models.Model):
-    # rut - Nombre - AppPaterno - appMaterno - fecha Nacimiento
-    # correo - telefono - direccion
-    rut = models.CharField(max_length=10, primary_key=True)
-    nombre = models.CharField(max_length=50, blank=False, null=False)
-    appPaterno = models.CharField(max_length=30, blank=False, null=False)
-    appMaterno = models.CharField(max_length=30, blank=False, null=False)
-    fechaNacimiento = models.DateField(blank=False, null=False)
-    telefono = models.CharField(max_length=10, blank=False, null=False)
-    direccion = models.CharField(max_length=50, blank=False, null=False)
+class tipoUsuario(models.Model):
+    idTipoUsuario = models.AutoField(
+        primary_key = True, db_column= 'idTipo', verbose_name ='id_tipoUsuario')
+    tipoUsuario = models.CharField(max_length = 20, blank = False, null = False)
 
     def __str__(self):
-        return str(self.nombre)+" "+str(self.appPaterno)+" "+str(self.appMaterno)
+        return str(self.tipoUsuario)
+
+class Usuario(models.Model):
+    # Nombre - AppPaterno -  correo - clave - direccion - telefono - Region - Provincia
+    # Comuna - Codigo postal
+
+    nombre = models.CharField(max_length=50, blank=False, null=False)
+    apaterno = models.CharField(max_length=30, blank=False, null=False)
+    amaterno = models.CharField(max_length=30, blank=False, null=False)
+    correo = models.EmailField(unique=True, blank=False, null=False, max_length=100)
+    clave = models.CharField(max_length=30, blank=False, null=False)
+    direccion = models.CharField(max_length=50, blank=False, null=False)
+    telefono = models.CharField(max_length=10, blank=False, null=False)
+    region = models.CharField(max_length=50, blank=False, null=False)
+    provincia = models.CharField(max_length=50, blank=False, null=False)
+    comuna = models.CharField(max_length=50, blank=False, null=False)
+    codpos = models.CharField(max_length=7, blank=False, null=False)
+    tipoUsuario = models.ForeignKey('tipoUsuario', on_delete=models.CASCADE, db_column='idTipo')
+    
+    def __str__(self):
+        return str(self.nombre)+" "+str(self.apaterno)+" "+str(self.amaterno)
--- Django/Botania/urls.py
+++ Django/Botania/urls.py
@@ -2,5 +2,20 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index', views.index, name='index'),    
+    path("index", views.index, name="index"),
+    path("iniciars", views.iniciars, name="iniciars"),
+    path("registrar", views.registrar, name="registrar"),
+    path("crud", views.crud, name="crud"),
+    path("userAdd", views.userAdd, name="userAdd"),
+    path("userDel/ <str:pk>", views.userDel, name="userDel"),
+    path("userEdit/ <str:pk>", views.userEdit, name="userEdit"),
+    path("userUpdate", views.userUpdate, name="userUpdate"),
+    path("formAdd", views.formAdd, name="formAdd"),
+    path("juegos", views.juegos, name="juegos"),
+    path("crudTipo", views.crudTipo, name="crudTipo"),
+    path("tipoAdd", views.tipoAdd, name="tipoAdd"),
+    path("tipoDel/ <str:pk>", views.tipoDel, name="tipoDel"),
+    path("tipoEdit/ <str:pk>", views.tipoEdit, name="tipoEdit"),
+    path("", views.login, name="login"),
+
 ]
